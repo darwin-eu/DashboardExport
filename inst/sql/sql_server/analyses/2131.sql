74bbe5c306664c73071b48a688e605ddc9af7cee
SofiaMp <58699690+SofiaMp@users.noreply.github.com>
2023-06-06 14:47:31 +0200

    additional analysis for observation and device_exposure

---
+++ inst/sql/sql_server/analyses/2131.sql
@@ -0,0 +1,21 @@
+-- 2131 Number of records by device_concept_id by age decile
+
+SELECT 
+	2131 AS analysis_id,
+    de.device_concept_id AS stratum_1,
+    FLOOR((YEAR(de.device_exposure_start_date) - p.year_of_birth) / 10) AS stratum_2,
+    CAST(NULL AS VARCHAR(255)) AS stratum_3,
+    CAST(NULL AS VARCHAR(255)) AS stratum_4,
+    CAST(NULL AS VARCHAR(255)) AS stratum_5,
+    COUNT_BIG(*) AS count_value
+FROM @cdm_database_schema.person p
+JOIN @cdm_database_schema.device_exposure de
+    ON p.person_id = co.person_id
+JOIN @cdm_database_schema.observation_period op
+    ON co.person_id = op.person_id
+    AND de.device_exposure_start_date >= op.observation_period_start_date
+    AND de.device_exposure_start_date <= op.observation_period_end_date
+WHERE device_concept_id > 0
+GROUP BY 
+    de.device_concept_id,
+    FLOOR((YEAR(de.device_exposure_start_date) - p.year_of_birth) / 10)
---
+++ inst/sql/sql_server/analyses/831.sql
@@ -0,0 +1,21 @@
+-- 831 Number of records by observation_concept_id by age decile
+
+SELECT
+    831 AS analysis_id,
+    o.observation_concept_id AS stratum_1,
+    FLOOR((YEAR(ob.observation_date) - p.year_of_birth) / 10) AS stratum_2,
+    CAST(NULL AS VARCHAR(255)) AS stratum_3,
+    CAST(NULL AS VARCHAR(255)) AS stratum_4,
+    CAST(NULL AS VARCHAR(255)) AS stratum_5,
+    COUNT_BIG(*) AS count_value
+FROM @cdm_database_schema.person p
+    JOIN @cdm_database_schema.observation o
+ON p.person_id = co.person_id
+    JOIN @cdm_database_schema.observation_period op
+    ON co.person_id = op.person_id
+    AND o.observation_date >= op.observation_period_start_date
+    AND o.observation_date <= op.observation_period_end_date
+WHERE observation_concept_id > 0 -- not in original 404 analysis
+GROUP BY
+    o.observation_concept_id,
+    FLOOR((YEAR(ob.observation_date) - p.year_of_birth) / 10)
