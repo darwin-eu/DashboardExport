35d01183c7dc83e8978a74645d321ee261203ccf
Maxim Moinat <MaximMoinat@users.noreply.github.com>
2025-01-21 07:29:24 +0100

    fix episode query (#60)

--- inst/sql/sql_server/analyses/2320.sql
+++ inst/sql/sql_server/analyses/2320.sql
@@ -1,23 +1,18 @@
 -- 2320	Number of episode records by episode start month
 
-INSERT INTO @results_database_schema.@results_table
 WITH rawData AS (
-SELECT 
-	YEAR(ep.episode_start_date) * 100 + MONTH(ep.episode_start_date) AS stratum_1,
-	COUNT_BIG(ep.person_id) AS count_value
-FROM 
-	@cdm_database_schema.episode ep
-JOIN 
-	@cdm_database_schema.observation_period op 
-ON 
-	ep.person_id = op.person_id
-AND 
-	ep.episode_start_date >= op.observation_period_start_date
-AND 
-	ep.episode_start_date <= op.observation_period_end_date
-GROUP BY 
-	YEAR(ep.episode_start_date) * 100 + MONTH(ep.episode_start_date)
+  SELECT 
+    YEAR(ep.episode_start_date) * 100 + MONTH(ep.episode_start_date) AS stratum_1,
+    COUNT_BIG(ep.person_id) AS count_value
+  FROM @cdm_database_schema.episode ep
+  JOIN @cdm_database_schema.observation_period op 
+    ON ep.person_id = op.person_id
+    AND ep.episode_start_date >= op.observation_period_start_date
+    AND ep.episode_start_date <= op.observation_period_end_date
+  GROUP BY 
+    YEAR(ep.episode_start_date) * 100 + MONTH(ep.episode_start_date)
 )
+INSERT INTO @results_database_schema.@results_table
 SELECT
 	2320 AS analysis_id,
 	CAST(stratum_1 AS VARCHAR(255)) AS stratum_1,
--- inst/sql/sql_server/analyses/2343.sql
+++ inst/sql/sql_server/analyses/2343.sql
@@ -2,54 +2,54 @@
 -- If day and/or month of birth are not given, then these are set to 1.
 
 WITH cte1 AS (
-    SELECT
-        person_id,
-        datefromparts(
-                year_of_birth,
-                coalesce(month_of_birth, 1),
-                coalesce(day_of_birth, 1)
-        ) as date_of_birth
-    FROM @cdm_database_schema.person
+  SELECT
+    person_id,
+    datefromparts(
+      year_of_birth,
+      coalesce(month_of_birth, 1),
+      coalesce(day_of_birth, 1)
+    ) as date_of_birth
+  FROM @cdm_database_schema.person
 ), cte2 as (
-    SELECT
-        p.person_id,
-        ep.episode_concept_id,
-        DATEDIFF(day, p.date_of_birth, ep.episode_start_date) AS age_days,
-        DATEDIFF(year, p.date_of_birth, ep.episode_start_date) AS age_years
-    FROM cte1 p
-             JOIN @cdm_database_schema.episode ep
+  SELECT
+    p.person_id,
+    ep.episode_concept_id,
+    DATEDIFF(day, p.date_of_birth, ep.episode_start_date) AS age_days,
+    DATEDIFF(year, p.date_of_birth, ep.episode_start_date) AS age_years
+  FROM cte1 p
+  JOIN @cdm_database_schema.episode ep
     ON p.person_id = ep.person_id
-        JOIN @cdm_database_schema.observation_period op
-        ON ep.person_id = op.person_id
-        AND ep.episode_start_date >= op.observation_period_start_date
-        AND ep.episode_start_date <= op.observation_period_end_date
-    WHERE DATEDIFF(year, p.date_of_birth, ep.episode_start_date) < 19
-      AND episode_concept_id != 0
+  JOIN @cdm_database_schema.observation_period op
+    ON ep.person_id = op.person_id
+    AND ep.episode_start_date >= op.observation_period_start_date
+    AND ep.episode_start_date <= op.observation_period_end_date
+  WHERE DATEDIFF(year, p.date_of_birth, ep.episode_start_date) < 19
+    AND episode_concept_id != 0
 ), cte3 as (
-    SELECT *,
-           case
-               when age_days < 0
-                   then 'Negative age'
-               when age_days < 28
-                   then 'Term newborn infants (0 to 27 days)*'
-               when age_years < 2
-                   then 'Infants and toddlers (28 days to 23 months)*'
-               when age_years < 12
-                   then 'Children (2 to 11 years)'
-               when age_years < 19
-                   then 'Adolescents (12 to 18 years)'
-               end as age_group
-    from cte2
+  SELECT *,
+    case
+      when age_days < 0
+          then 'Negative age'
+      when age_days < 28
+          then 'Term newborn infants (0 to 27 days)*'
+      when age_years < 2
+          then 'Infants and toddlers (28 days to 23 months)*'
+      when age_years < 12
+          then 'Children (2 to 11 years)'
+      when age_years < 19
+          then 'Adolescents (12 to 18 years)'
+    end as age_group
+  from cte2
 )
 INSERT INTO @results_database_schema.@results_table
 select
-    2343 as analysis_id,
-    device_concept_id as stratum_1,
-    age_group as stratum_2,
-    CAST(NULL AS VARCHAR(255)) AS stratum_3,
-    CAST(NULL AS VARCHAR(255)) AS stratum_4,
-    CAST(NULL AS VARCHAR(255)) AS stratum_5,
-    count_big(*) as count_value
+  2343 as analysis_id,
+  episode_concept_id as stratum_1,
+  age_group as stratum_2,
+  CAST(NULL AS VARCHAR(255)) AS stratum_3,
+  CAST(NULL AS VARCHAR(255)) AS stratum_4,
+  CAST(NULL AS VARCHAR(255)) AS stratum_5,
+  count_big(*) as count_value
 from cte3
 group by episode_concept_id, age_group
 ;
