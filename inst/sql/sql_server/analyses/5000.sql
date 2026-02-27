845093bed877333de4f441f62a401f51a19c0864
LP-Glueck <liam@thehyve.nl>
2026-02-27 12:39:02 +0100

    adding cdm_source ranking to analysis 5000

--- inst/sql/sql_server/analyses/5000.sql
+++ inst/sql/sql_server/analyses/5000.sql
@@ -1,4 +1,12 @@
 -- CDM Source
+WITH ranked_cdm_source AS (
+    SELECT
+        cdm_source_name,
+        cdm_release_date,
+        cdm_version,
+        ROW_NUMBER() OVER (ORDER BY cdm_release_date DESC) AS rn
+    FROM @cdm_database_schema.cdm_source
+)
 INSERT INTO @results_database_schema.@results_table
     SELECT
         5000 as analysis_id,
@@ -8,7 +16,7 @@ INSERT INTO @results_database_schema.@results_table
         cdm_version as stratum_4,
         vocabulary.vocabulary_version as stratum_5,
         999 as count_value -- needs to be > min_cell_count to pass export
-    FROM @cdm_database_schema.cdm_source
+    FROM ranked_cdm_source
     cross join (
         select vocabulary_version 
         from @cdm_database_schema.vocabulary
