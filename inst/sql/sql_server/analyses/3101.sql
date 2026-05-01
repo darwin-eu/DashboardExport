b3d2c2d4f9c03d0a5e5d3207cead992fd46c4b06
Maxim Moinat <maximmoinat@gmail.com>
2026-05-01 09:07:34 +0200

    add initial pet analyses

---
+++ inst/sql/sql_server/analyses/3101.sql
@@ -0,0 +1,23 @@
+-- 3101 Number of records by pregnancy outcome
+
+INSERT INTO @results_database_schema.@results_table
+SELECT 
+	3101 AS analysis_id,
+	CAST(pet.pregnancy_outcome AS VARCHAR(255)) AS stratum_1,
+	CAST(NULL AS VARCHAR(255)) AS stratum_2,
+	CAST(NULL AS VARCHAR(255)) AS stratum_3,
+	CAST(NULL AS VARCHAR(255)) AS stratum_4,
+	CAST(NULL AS VARCHAR(255)) AS stratum_5,
+	COUNT_BIG(pet.person_id) AS count_value
+FROM 
+	@cdm_database_schema.pregnancy pet
+JOIN 
+	@cdm_database_schema.observation_period op 
+ON 
+	pet.person_id = op.person_id
+AND 
+	pet.pregnancy_start_date >= op.observation_period_start_date
+AND 
+	pet.pregnancy_start_date <= op.observation_period_end_date
+GROUP BY 
+	pet.pregnancy_outcome;
---
+++ inst/sql/sql_server/analyses/3106.sql
@@ -0,0 +1,77 @@
+-- 3106 Distribution of age at first pregnancy
+
+with cte as (
+  SELECT 
+    pet.pregnancy_start_year - p.year_of_birth AS count_value
+  FROM 
+    @cdm_database_schema.person p
+  JOIN (
+    SELECT 
+      pet.person_id
+      MIN(YEAR(pet.pregnancy_start_date)) AS pregnancy_start_year
+    FROM 
+      @cdm_database_schema.pregnancy pet
+    JOIN 
+      @cdm_database_schema.observation_period op 
+    ON 
+      pet.person_id = op.person_id
+    AND 
+      pet.pregnancy_start_date >= op.observation_period_start_date
+    AND 
+      pet.pregnancy_start_date <= op.observation_period_end_date
+    GROUP BY 
+      pet.person_id
+    ) pet 
+  ON 
+    p.person_id = pet.person_id
+), overallStats as
+(
+  SELECT
+    CAST(avg(1.0 * count_value) AS FLOAT) as avg_value,
+    CAST(stdev(count_value) AS FLOAT) as stdev_value,
+    min(count_value) as min_value,
+    max(count_value) as max_value,
+    count_big(*) as total
+  FROM cte
+),
+statsView (count_value, total, rn) as
+(
+  SELECT
+    count_value,
+    count_big(*) as total,
+    row_number() over (order by count_value) as rn
+  FROM cte
+  GROUP BY count_value
+),
+priorStats (count_value, total, accumulated) as
+(
+  SELECT
+    s.count_value,
+    s.total,
+    sum(p.total) as accumulated
+  FROM statsView s
+  JOIN statsView p on s.stratum1_id = p.stratum1_id and s.stratum2_id = p.stratum2_id and p.rn <= s.rn
+  GROUP BY s.count_value, s.total, s.rn
+)
+INSERT INTO @results_database_schema.@results_table_dist
+SELECT 
+  3106 as analysis_id,
+  CAST(NULL AS VARCHAR(255)) AS stratum_1,
+  CAST(NULL AS VARCHAR(255)) AS stratum_2,
+  CAST(NULL AS VARCHAR(255)) AS stratum_3,
+	CAST(NULL AS VARCHAR(255)) AS stratum_4,
+	CAST(NULL AS VARCHAR(255)) AS stratum_5,
+  o.total as count_value,
+  o.min_value,
+	o.max_value,
+	o.avg_value,
+	o.stdev_value,
+	MIN(case when p.accumulated >= .50 * o.total then count_value else o.max_value end) as median_value,
+	MIN(case when p.accumulated >= .10 * o.total then count_value else o.max_value end) as p10_value,
+	MIN(case when p.accumulated >= .25 * o.total then count_value else o.max_value end) as p25_value,
+	MIN(case when p.accumulated >= .75 * o.total then count_value else o.max_value end) as p75_value,
+	MIN(case when p.accumulated >= .90 * o.total then count_value else o.max_value end) as p90_value
+FROM priorStats p
+JOIN overallStats o on p.stratum1_id = o.stratum1_id and p.stratum2_id = o.stratum2_id 
+GROUP BY o.total, o.min_value, o.max_value, o.avg_value, o.stdev_value
+;
